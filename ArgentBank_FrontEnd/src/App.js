3c03be14f62e2374d29ad9056cf06f43e81a38a8
mathis marichy <marichy.pro@gmail.com>
2024-02-09 14:19:08 +0100

    Redirect bad link to homepage | Removing duplicate app.js

--- ArgentBank_FrontEnd/src/App.js
+++ ArgentBank_FrontEnd/src/App.js
@@ -39,6 +39,7 @@ function App() {
               path="/profile"
               element={isConnected ? <Profile /> : <Navigate to="/login" />}
             />
+            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </main>
         <Footer />
